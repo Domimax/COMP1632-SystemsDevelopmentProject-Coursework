d4d0723af915e8a76d015b6dbd7ddd7990d0a6c6
rn6706a <44876350+rn6706a@users.noreply.github.com>
2019-03-06 12:00:22 +0000

    Add files via upload
    Added make booking functionality

---
+++ COMP1632-SystemsDevelopmentProject-Coursework-master/SystemsDevProject/SystemsDevProject/DBSingleton.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+
+namespace SystemsDevProject
+{
+    // A Database class which uses a Singleton pattern to only allow a single instance of itself and is used for interaction
+    //with a database.
+    internal sealed class DBSingleton
+    {
+        //A lock object to make the accessing of a single instance thread safe.
+        private static readonly object padlock = new object();
+        //A Singleton instance of the class.
+        private static DBSingleton instance = null;
+
+        //Property used for the retrieval of the Singleton instance.
+        public static DBSingleton GetDBSingletonInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DBSingleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        // Constructor. Used only once.
+        private DBSingleton()
+        {
+
+        }
+
+        // Method returns a database connection.
+        /*
+        private OleDbConnection GetOleDbConnection()
+        {
+            String connection = @"Provider=Microsoft.JET.OLEDB.4.0; 
+				 Data Source =" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"DB\SystemsDevProjectDB.mdb");
+            OleDbConnection myConnection = new OleDbConnection(connection);
+            return myConnection;
+        }
+        */
+        /*
+        //Method returns a List of all questions in a database.
+        public List<Question> GetQuestions(string amount)
+        {
+            List<Question> questions = new List<Question>();
+            OleDbConnection connection = GetOleDbConnection();
+            //Selects the amount of questions selected by the user from a database.
+            string query = "SELECT TOP  " + amount.ToString() + " * FROM Question;";
+            OleDbCommand questionCommand = new OleDbCommand(query, connection);
+            try
+            {
+                connection.Open();
+                OleDbDataReader questionReader = questionCommand.ExecuteReader();
+                while (questionReader.Read())
+                {
+                    int id = (int)questionReader["ID"];
+                    string questionText = (string)questionReader["QuestionText"];
+                    string questionType = (string)questionReader["QuestionType"];
+                    Question newQuestion = null;
+                    //A switch case is used to determine which kind of question has been extracted from a database.
+                    switch (questionType)
+                    {
+                        case "InputAnswer":
+                            newQuestion = new InputAnswerQuestion(id, questionText, questionType, GetAnswers(id, connection));
+                            break;
+                        case "MultipleChoice":
+                            newQuestion = new MultipleChoiceQuestion(id, questionText, questionType, GetAnswers(id, connection));
+                            break;
+                        case "Music":
+                            newQuestion = new MusicQuestion(id, questionText, questionType, GetAnswers(id, connection));
+                            break;
+                        case "Picture":
+                            newQuestion = new PictureQuestion(id, questionText, questionType, GetAnswers(id, connection));
+                            break;
+                        case "YesOrNo":
+                            newQuestion = new YesOrNoQuestion(id, questionText, questionType, GetAnswers(id, connection));
+                            break;
+                    }
+                    //If an abnormal question type is discovered, an exception is thrown.
+                    if (newQuestion != null)
+                    {
+                        questions.Add(newQuestion);
+                    }
+                    else
+                    {
+                        throw new Exception("newQuestion object is null.");
+                    }
+                }
+                //Checking if there are any questions in the database at all.
+                if (questions.Count == 0)
+                {
+                    throw new Exception("Database is empty.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return questions;
+        }
+
+        //Returns a list of answers by using a provided id of a specific question.
+        private List<Answer> GetAnswers(int questionId, OleDbConnection connection)
+        {
+            List<Answer> answers = new List<Answer>();
+            //Query to select all answers which have a specific question_id.
+            string query = "SELECT * FROM Answer WHERE Question_Id = " + questionId + ";";
+            OleDbCommand answerCommand = new OleDbCommand(query, connection);
+            try
+            {
+                OleDbDataReader answerReader = answerCommand.ExecuteReader();
+                while (answerReader.Read())
+                {
+                    int id = (int)answerReader["ID"];
+                    string answerText = (string)answerReader["AnswerText"];
+                    bool correct = (bool)answerReader["Correct"];
+                    answers.Add(new Answer(id, answerText, correct));
+                }
+                //Checks if there are any answers at all.
+                if (answers.Count == 0)
+                {
+                    throw new Exception("Database is empty.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex);
+            }
+            return answers;
+        }
+
+        //Returns a list of all the scores stored in the database to show in the leaderboards.
+        public List<Score> GetScores()
+        {
+            List<Score> scores = new List<Score>();
+            OleDbConnection connection = GetOleDbConnection();
+            //Nested Select query used to retrieve top 15 scores based on percentage of correctly answered questions and the time it took
+            //to complete the quiz.
+            string query = "SELECT TOP 15 * FROM (SELECT * FROM Score ORDER BY CorrectPercentage DESC, TimeTook ASC, PlayerName DESC);";
+            OleDbCommand scoreCommand = new OleDbCommand(query, connection);
+            try
+            {
+                connection.Open();
+                OleDbDataReader scoreReader = scoreCommand.ExecuteReader();
+                while (scoreReader.Read())
+                {
+                    int id = (int)scoreReader["ID"];
+                    double correctPercentage = (int)scoreReader["CorrectPercentage"];
+                    DateTime completed = (DateTime)scoreReader["Completed"];
+                    string playerName = (string)scoreReader["PlayerName"];
+                    int timeTook = (int)scoreReader["TimeTook"];
+                    int correctAnswers = (int)scoreReader["CorrectAnswers"];
+                    int questionNumber = (int)scoreReader["QuestionNumber"];
+                    Score newScore = new Score(id, correctAnswers, correctPercentage,
+                        playerName, completed, timeTook, questionNumber);
+                    //Check if it is a suitable score.
+                    if (newScore != null)
+                    {
+                        scores.Add(newScore);
+                    }
+                    else
+                    {
+                        throw new Exception("newScore object is null.");
+                    }
+                }
+                //Check if there are any score at all.
+                if (scores.Count == 0)
+                {
+                    throw new Exception("Database is empty.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return scores;
+        }
+        */
+        //Get connection MY METHOD
+        public static OleDbConnection GetConnection()
+        {
+            String connectionString;
+            connectionString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source=I:\SystemsDev.mdb";
+            return new OleDbConnection(connectionString);
+        }
+        //Method to add a review to the database
+        public void WriteReview(string name, string review, DateTime date)
+        {
+            OleDbConnection connection = GetConnection();
+            //OleDbConnection connection = GetOleDbConnection();
+            string query = "INSERT INTO Reviews( [Date and Time of attendance], [Name of Performance], Review) VALUES( '" + date + "' , " + review + " )";
+            OleDbCommand cmd = new OleDbCommand(query, connection);
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed. Error " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public List<String> GetPerformance(string s, List<String> list)
+        {
+            OleDbConnection connection = GetConnection();
+            string query = ("SELECT * FROM Performances WHERE [Type of performance]='" + s + "' ");
+            OleDbCommand cmd = new OleDbCommand(query, connection);
+            OleDbDataReader reader;
+            list.Add("test");
+            list.Clear();
+            try
+            {
+                connection.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(reader["Name of"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error. Could not perform the requested action. Error: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return list;
+        }
+            /*
+        //Method is used to store the score of a completed quiz to a database.
+        public void AddScore(Score score)
+        {
+            OleDbConnection connection = GetOleDbConnection();
+            //Query used to find the highest id in the table.
+            string query = "SELECT ID FROM Score;";
+            OleDbCommand maxIDCommand = new OleDbCommand(query, connection);
+            int maxID = 0;
+            try
+            {
+                connection.Open();
+                OleDbDataReader maxIDReader = maxIDCommand.ExecuteReader();
+                while (maxIDReader.Read())
+                {
+                    if (maxID < (int)maxIDReader["ID"])
+                    {
+                        maxID = (int)maxIDReader["ID"];
+                    }
+                }
+                maxID++;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //A query used to insert a new tuple into the table.
+            query = "INSERT INTO Score(ID, CorrectPercentage, Completed, PlayerName, TimeTook, CorrectAnswers, QuestionNumber)" +
+                "VALUES (" + maxID + " , " + score.CorrectPercentage + " , #" + score.Completed.Date + "# , '" +
+                 score.PlayerName + "' , " + score.TimeTook + " , " + score.CorrectAnswers + " , " + score.QuestionNumber + ");";
+            OleDbCommand scoreCommand = new OleDbCommand(query, connection);
+            try
+            {
+                connection.Open();
+                scoreCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    */
+    }
+}
---
+++ COMP1632-SystemsDevelopmentProject-Coursework-master/SystemsDevProject/SystemsDevProject/ReviewForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SystemsDevProject
+{
+    partial class ReviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 276);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(564, 160);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name of play/performance";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date attended";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(231, 100);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(231, 173);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(200, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Your rating";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Please enter your comments below";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 472);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ReviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 528);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "ReviewForm";
+            this.Text = "ReviewForm";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
