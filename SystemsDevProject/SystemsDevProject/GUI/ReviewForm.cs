eba35fabbf72cb7e5cb80b7d90f6418a89c4bc0a
rn6706a <44876350+rn6706a@users.noreply.github.com>
2019-04-01 16:29:18 +0100

    Add files via upload

--- SystemsDevProject/SystemsDevProject/GUI/ReviewForm.cs
+++ SystemsDevProject/SystemsDevProject/GUI/ReviewForm.cs
@@ -11,7 +11,6 @@ using System.Windows.Forms;
 namespace SystemsDevProject
 {
     public partial class ReviewForm : Form
-<<<<<<< HEAD
     {
         List<Play> plays;
         Play currentPlay;
@@ -97,37 +96,5 @@ namespace SystemsDevProject
                 label10.Text = "You've given this performance: 5/5";
             }
         }
-=======
-    {
-        public ReviewForm()
-        {
-            InitializeComponent();
-        }
-
-        //coding submit button
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (comboBox1.SelectedIndex.ToString() != null && dateTimePicker1.Value != null && richTextBox1.Text != null && richTextBox1.Text != "")
-            {
-                string name = comboBox1.SelectedIndex.ToString();
-                string review = richTextBox1.Text;
-                DateTime date = dateTimePicker1.Value;
-                try
-                {
-                    DBSingleton.GetDBSingletonInstance.WriteReview(name, review, date);
-                    MessageBox.Show("Thank you. Your review has been saved");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Unable to connect to database. Error: " + ex);
-                }
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("Please ensure all fields have been filled out correctly");
-            }
-        }
->>>>>>> parent of 02bcbb9... Final Maks' work
     }
 }
---
+++ SystemsDevProject/SystemsDevProject/UserFactory.cs
@@ -0,0 +1,27 @@
+﻿using SystemsDevProject.Model;
+
+namespace SystemsDevProject
+{
+    public class UserFactory
+    {
+        public static User CreateUser(string userType)
+        {
+            if (userType == "Employee")
+            {
+                return new Employee();
+            }
+            else if (userType == "Agency")
+            {
+                return new Agency();
+            }
+            else if (userType == "Customer")
+            {
+                return new Customer();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
